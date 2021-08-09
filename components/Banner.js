5a39176f00e730ecb68c7fa18c275d12292ede4f
mamba-dev-KE <josephmaramba50@gmail.com>
2021-08-09 21:13:59 +0300

    changes on banner font color

--- components/Banner.js
+++ components/Banner.js
@@ -9,7 +9,9 @@ function Banner() {
 				objectFit="cover"
 			/>
 			<div className="absolute top-1/2 w-full text-center">
-				<p className="text-sm sm:text-lg text-white">Not sure where to go? Perfect.</p>
+				<p className=" text-lg sm:text-lg text-white ">
+					Not sure where to go? Perfect.
+				</p>
 				<button className="bg-white px-10 py-4 text-purple-500 shadow-md rounded-full font-bold my-4 hover:shadow-xl active:scale-90 transition duration-150">
 					I'm flexible
 				</button>
