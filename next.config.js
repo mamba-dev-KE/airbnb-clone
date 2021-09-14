d53510f790483c5a969acc54a1bda52575ee2c3b
mamba-dev-KE <josephmaramba50@gmail.com>
2021-09-14 11:23:43 +0300

    changing env variables

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
 		domains: ["links.papareact.com", "i.imgur.com"],
 	},
 	env: {
-		mapbox_key: process.env.API_KEY,
+		mapbox_key:
+			"pk.eyJ1IjoibWFtYmEtZGV2LWtlIiwiYSI6ImNrc3Zma2h6OTFwZGEydW1kM3F6N2R0NmEifQ.LCvIzUxXtiJP8cIxi6 - Gkg",
 	},
 };
